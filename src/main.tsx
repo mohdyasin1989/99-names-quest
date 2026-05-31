61a7f73eb782afcdcc701888b02c1593deff4572
mohdyasin1989 <mohdyasin1989@gmail.com>
2026-05-31 22:13:57 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import { GameProvider } from './context/GameContext'
 import './index.css'
@@ -10,6 +9,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <GameProvider>
       <App />
     </GameProvider>
-    <Analytics />
   </React.StrictMode>,
 )
